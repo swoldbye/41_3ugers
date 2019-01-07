d93494d141ce8e81b3d7941cb17937c895048f92
Simon Woldbye-Lyng <swoldbye@gmail.com>
2019-01-07 16:15:49 +0100

    Dice fixed

--- src/Entities/Dice.java
+++ src/Entities/Dice.java
@@ -1,81 +1,15 @@
 package Entities;
+
 import java.util.Random;
 
-public class Dice{
+public class Dice {
 
-    //Constructor of player-object
-    private int sides;
-    /**
-     * Dice is initiated with a Sides Attribute
-     *
-     * @see sides                   Sides of the dice, "how many different values can it show"
-     *
-     * @param sides                 Sides of the dice, "how many different values can it show"
-     *
-     * @return void
-     * @author Hans P Byager , Tristan
-     * @date   08.11.2018
-     */
-    public Dice(int sides){
-        this.sides = this.sides;
-    }
 
-    /**
-     * Dice needs to recieve an Integer Array after being initiated, such that it knows what values are on what sides of the dice object
-     *
-     * @see sidesNum                  different values on different sides of the dice
-     *
-     * @param SidesNum                different values on different sides of the dice
-     *
-     * @return void
-     * @author Hans P Byager , Tristan
-     * @date   08.11.2018
-     */
-    private int[] SidesNum = new int[sides];
-    public void DiceSidesNumDecider(int[] SidesNum){
-        this.SidesNum = SidesNum;
-    }
-
-    /**
-     * A roll funktion that rolls the dice, using a random funktion, to decide what side it lands on, and then reads that sides value to
-     * return the value of the rolled side.
-     *
-     * @return int
-     * @author Hans P Byager , Tristan
-     * @date   08.11.2018
-     */
-    public int roll(){
+    public int roll2() {
         Random rand = new Random(); //Make an instance of Random
-        int roll = rand.nextInt(6)+1; //Random int between [0;max[ +1 ... therefore [1;max]
-
+        int roll = rand.nextInt(6) + 1; //Random int between [0;max[ +1 ... therefore [1;max]
+        int roll2 = rand.nextInt(6) + 1;
         //int equalDiceValue = SidesNum[roll-1];
-        return roll;
+        return roll+roll2;
     }
-
-    //get & set Sides
-    /**
-     * GetSides returns an Integer of the number of Sides on the Dice
-     *
-     * @return int
-     * @author Tristan
-     * @date   08.11.2018
-     */
-    public int getSides()
-    {
-        return sides;
-    }
-    /**
-     * setSides gives the Dice a new Sides Value
-     *
-     * @param Sides         an integer that indicates the number of sides on the dice.
-     * @return Void
-     * @author Tristan
-     * @date   08.11.2018
-     */
-    public void setSides(int Sides)
-    {
-        this.sides = Sides;
-    }
-
-
 }
