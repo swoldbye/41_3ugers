0d343e56b3be7bab04640c0cee817f2ef8cd1a1d
Tristan <tris2012@live.dk>
2019-01-08 10:42:44 +0100

    Field_Abstract and Field_Property initial draft of code.

--- src/Entities/Field_Abstract.java
+++ src/Entities/Field_Abstract.java
@@ -2,6 +2,29 @@ package Entities;
 
 public class Field_Abstract {
 
-    private int fieldIndex;
+    protected int fieldIndex;
+    protected String fieldName;
 
+    public Field_Abstract(int fieldIndex, String fieldName) {
+        this.fieldIndex = fieldIndex;
+        this.fieldName = fieldName;
+    }
+
+
+    //Getter & Setter methods
+    public int getFieldIndex() {
+        return fieldIndex;
+    }
+
+    public void setFieldIndex(int fieldIndex) {
+        this.fieldIndex = fieldIndex;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
 }
--- src/Entities/Field_Property.java
+++ src/Entities/Field_Property.java
@@ -1,4 +1,52 @@
 package Entities;
 
-public class Field_Property {
+public class Field_Property extends Field_Abstract{
+
+    private int price;
+    private int rent;
+    private int house;
+    private int hotel;
+
+    //Constructor
+    public Field_Property(int fieldIndex, String fieldName, int price, int rent, int house, int hotel) {
+        super(fieldIndex, fieldName);
+        this.price = price;
+        this.rent = rent;
+        this.house = house;
+        this.hotel = hotel;
+    }
+
+
+    //Getter & Setter methods
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getRent() {
+        return rent;
+    }
+
+    public void setRent(int rent) {
+        this.rent = rent;
+    }
+
+    public int getHouse() {
+        return house;
+    }
+
+    public void setHouse(int house) {
+        this.house = house;
+    }
+
+    public int getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(int hotel) {
+        this.hotel = hotel;
+    }
 }
