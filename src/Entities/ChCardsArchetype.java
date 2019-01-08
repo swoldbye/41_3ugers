44ff3bb47a5e1fe26516897a40fd922ad08deecd
Simon Woldbye-Lyng <swoldbye@gmail.com>
2019-01-08 10:24:29 +0100

    Oprettet: ChCardsArchetype, ChCardsList. Ændret: PlayerList

---
+++ src/Entities/ChCardsArchetype.java
@@ -0,0 +1,15 @@
+package Entities;
+
+public class ChCardsArchetype {
+
+    private String cardName;
+    private int cardEffect;
+
+
+    public ChCardsArchetype(String cardName, int cardEffect) {
+        this.cardName = cardName;
+        this.cardEffect = cardEffect;
+    }
+}
+
+
--- src/Entities/PlayerList.java
+++ src/Entities/PlayerList.java
@@ -2,4 +2,11 @@ package Entities;
 
 public class PlayerList {
 
+    private int playerCount;
+
+    public PlayerList(int playerCount) {
+        this.playerCount = playerCount;
+    }
+
+
 }
