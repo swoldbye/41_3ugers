bac388b13e5009f767b459fcd6d04471a84767c2
Emil <emil514k@gmai.com>
2019-01-10 13:02:09 +0100

    ChCardsList Almost done ChCardsArchetype altered

--- src/Entities/ChCardsList.java
+++ src/Entities/ChCardsList.java
@@ -1,6 +1,64 @@
 package Entities;
+import Entities.ChCardsArchetype;
+import gui_fields.GUI_Chance;
 
-public class ChCardsList {
+
+public final class ChCardsList {
+    ChCardsList() {
+    }
 
 
-}
+    public static ChCardsArchetype[] fieldInitializer() {
+        // Now contains the relevant strings for fields
+        ChCardsArchetype[] cards = new ChCardsArchetype[][40];
+        int i = 0;
+
+        //First color is fieldColor, Second color is fieldtext/name..
+
+
+
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag 100", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal 100", 1, -100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+        cards[i] = new ChCardsArchetype("Chance", "Betal", 1, 100);
+
+        return cards;
+    }}
+
