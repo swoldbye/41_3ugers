2f7cd0c7ceb928ecabe043f7f028a6391f3e3096
Emil <emil514k@gmai.com>
2019-01-10 14:01:04 +0100

    ChCardsList being fixed

--- src/Entities/ChCardsList.java
+++ src/Entities/ChCardsList.java
@@ -10,29 +10,29 @@ public final class ChCardsList {
 
     public static ChCardsArchetype[] fieldInitializer() {
         // Now contains the relevant strings for fields
-        ChCardsArchetype[] cards = new ChCardsArchetype[][40];
+        ChCardsArchetype[] cards = new ChCardsArchetype[40];
         int i = 0;
 
-        //First color is fieldColor, Second color is fieldtext/name..
 
 
 
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag 100", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Betal 100", 1, -100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
-        cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
+
+        cards[i++] = new ChCardsArchetype("Chance", "De har vundet i klasse lotteriet. Modtag kr. 500", 1, 500);
+        cards[i++] = new ChCardsArchetype("Chance", "Deres præmieobligation er udtrukket. de modtager kr. 1000 af banken.", 1, 1000);
+        cards[i++] = new ChCardsArchetype("Chance", "De havde en række med elleve rigtige i tipning. Modtag kr. 1000.", 1, 1000);
+        cards[i++] = new ChCardsArchetype("Chance", "Deres præmieobligation er udtrukket. De modtager kr. 1000 af banken.", 1, 1000);
+        cards[i++] = new ChCardsArchetype("Chance", "De har vundet i Klasseloterriet. Modtag kr. 500.", 1, 500);
+        cards[i++] = new ChCardsArchetype("Chance", "Modtag udbytte af Deres aktier - kr. 1000.", 1, 1000);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal for vognvask og smøring kr. 300", 1, -300);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal kr.200 for levering af 2 kasser øl.", 1, -200);
+        cards[i++] = new ChCardsArchetype("Chance", "Værdien af egen fra nyttehaven udgør kr. 200, som De modtager af banken.", 1, 200);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal kr. 3000 for reparation af Deres vogn.", 1, -3000);
+        cards[i++] = new ChCardsArchetype("Chance", "Betal Deres bilforsikring kr. 1000", 1, -1000);
+        cards[i++] = new ChCardsArchetype("Chance", "De har solgt nogle gamle møbler på auktion. Modtag kr. 1000 af banken.", 1, 1000);
+        cards[i++] = new ChCardsArchetype("Chance", "De modtager Deres aktieudbytte, modtag kr. 1000 af banken.", 1, 1000);
+        cards[i++] = new ChCardsArchetype("Chance", "Grundet dyrtiden har de fået gageforhøjelse. modtag kr. 1000", 1, 1000);
+        cards[i++] = new ChCardsArchetype("Chance", "De har købt 4 nye dæk til deres vogn. betal kr. 1000", 1, 1000);
+        cards[i++] = new ChCardsArchetype("Chance", "Kommunen har eftergivet etkvartals skat. Hæv i banken kr. 3000", 1, 100);
         cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
         cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
         cards[i++] = new ChCardsArchetype("Chance", "Modtag", 1, 100);
