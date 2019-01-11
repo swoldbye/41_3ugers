052400b41fd969e85491996811453f9a119ff00b
Tristan <tris2012@live.dk>
2019-01-11 09:33:47 +0100

    FieldList initialization added to C_init.

--- src/Controllers/C_Initializer.java
+++ src/Controllers/C_Initializer.java
@@ -1,9 +1,11 @@
 package Controllers;
 
 import Boundary.GUI_Monopoly;
+import Entities.Field_Abstract;
 import Entities.PlayerArchetype;
 import Entities.PlayerList;
 import gui_fields.GUI_Player;
+import Entities.FieldList;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -14,15 +16,20 @@ public class C_Initializer {
         GUI_Monopoly guiBoard = new GUI_Monopoly();
         PlayerList playerList = new PlayerList();
 
+
         guiBoard.boardSetup();
         int playerAmount = guiBoard.playerAmount();
         String[] playerNames = guiBoard.playerNames(playerAmount);
+
         ArrayList<PlayerArchetype> c_playerArr = playerList.playerListInitializer(playerNames, playerAmount);
         GUI_Player[] c_GUIPlayerArr = guiBoard.InitializePlayersGUI(c_playerArr);
 
 
         GameTurn startGame = new GameTurn();
         startGame.checkTurn(c_playerArr, c_GUIPlayerArr);
+
+        //FieldList initialization
+        Field_Abstract[] fieldList =  FieldList.createFieldList();
     }
 
 }
