8f74c9a65963e1dd78f7c577d7f9d9000f382ffd
Emil <emil514k@gmai.com>
2019-01-13 15:31:37 +0100

    Chcane deck of cards first iteration done

--- src/GameLogic/L_ChanceCard.java
+++ src/GameLogic/L_ChanceCard.java
@@ -1,9 +1,16 @@
 package GameLogic;
 
+import java.util.Random;
 public class L_ChanceCard {
 
     public int chanceCardDeck(int position,int playerBalance){
+        Random cardRand = new Random();
+        int card = cardRand.nextInt(40);
 
-        return position;
+        return card;
     }
+
+
+
+
 }
