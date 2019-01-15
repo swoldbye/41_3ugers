4560eedf6c0fc01e4502480a860f7d5ed8ac3388
Simon Woldbye-Lyng <swoldbye@gmail.com>
2019-01-15 12:06:16 +0100

    groupsize hardcode.

--- src/GameLogic/L_PropertyManagement.java
+++ src/GameLogic/L_PropertyManagement.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class L_PropertyManagement {
 
     public void ownsGroup(PlayerArchetype[] playerArr, int playerId, Field_Abstract[] properties){
-        for(int i = 0; i < properties.length; i++){
+
 
         }
     }
