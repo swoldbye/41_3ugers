1a69426e8219d2b7a6084cec0de812e33f67249f
mcmalloy <xomagametv@gmail.com>
2019-01-18 13:08:36 +0100

    changed names of a method

--- src/Controllers/GameTurn.java
+++ src/Controllers/GameTurn.java
@@ -41,11 +41,13 @@ public class GameTurn {
                 if (getTurn == true) {
                     // Check the current position of the player before he rolls the dice
                     int oldPosition = playerArr.get(i).getPosition();
+                    // check groupsOwnedAmounted[] and increment groupsOwned[] if you have all in the group
                     Logic_propertymanagement.ownsGroupIncrement(playerArr, fieldArr, i);
-                    // 2) If the player owns all properties within one group, ask if he wants to buy houses
 
+                    // If the player owns all properties within one group
                     boolean ownsGroup = Logic_propertymanagement.ownsGroup(playerArr, fieldArr, i);
                     if (ownsGroup == true) {
+                        //
                         int buildField = Logic_propertymanagement.wantToBuy(playerArr, guiInstance, i, groupIndexes, fieldArr);
                         if(buildField > 0){
                             int houseAmount = guiInstance.houseAmount(fieldArr, buildField, playerArr, i);
@@ -61,8 +63,7 @@ public class GameTurn {
 
                     // 4) Checks if the player crosses the start
                     Logic_checkfield.checkStart(playerArr, oldPosition, i, roll1, roll2, gui_playerList);
-                    int actualPosition = Logic_checkfield.setNewPosition(playerArr, oldPosition, i, roll1, roll2);
-                    guiInstance.movePlayer(i, oldPosition, actualPosition, gui_playerList); // Moves the player in gui
+                    int actualPosition = Logic_checkfield.setNewPosition(playerArr, oldPosition, i, roll1, roll2,gui_playerList);
                     Logic_checkfield.checkPosition(playerArr, i, actualPosition, fieldArr, gui_playerList);
                     if (playerArr.get(i).getBalance() < 0) {
 
